d49e7ebc0da6515583828e60cf2c7f996fa45ad0
Cybersport <Cybersport@26kadr.internal>
2024-10-24 14:51:53 +0300

    Your commit text

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import './App.css';
 import logo from '../src/Images/logo.svg'
 import search from '../src/Images/search.svg'
 import cart from '../src/Images/cart.svg'
+import backgroud from '../src/Images/Background.png'
+import fruits from '../src/Images/Fruits.png'
 
 function App() {
   return (
     <>
+    <div className="container">
     <div className='header'>
       <img src={logo} alt="logo"></img>
       <p>React</p>
@@ -25,6 +28,14 @@ function App() {
         <img src={cart} alt=''/>
         <p>cart(0)</p>
       </div>
+    </div>
+    </div>
+
+    <div className="container_body">
+    <div className='body'>
+      <img src={backgroud} alt="back"/>
+      <img src={fruits} alt="fruits" />
+    </div>
     </div>
     </>
   );
